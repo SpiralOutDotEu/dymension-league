bbb8edf47502994f5cfe8b5274fdb0e870122d97
Jumaru <spiralout.eu@gmail.com>
2024-05-12 01:17:42 +0300

    refactor to constants file

--- frontend/app/hooks/useCosmoShips.ts
+++ frontend/app/hooks/useCosmoShips.ts
@@ -1,8 +1,7 @@
 import { ethers } from 'ethers';
 import CosmoShips from '../artifacts/contracts/CosmoShips.json';
 import tokenData from '../artifacts/proofs/proofs_0xcba72fb67462937b6fa3a41e7bbad36cf169815ea7fe65f8a4b85fd8f5facb28.json';
-
-const contractAddress = '0x3a906E15AFFb6923D62dfCB5923495cad60A712b';
+import { COSMOSHIPS_ADDRESS } from '../utils/constants';
 
 interface UseCosmoShipsHook {
     mint: (tokenId: number, signer: ethers.Signer) => Promise<void>;
@@ -12,7 +11,7 @@ const useCosmoShips = (): UseCosmoShipsHook => {
     
 
     const mint = async (tokenId: number, signer: ethers.Signer) => {
-        const contract = new ethers.Contract(contractAddress, CosmoShips.abi, signer);
+        const contract = new ethers.Contract(COSMOSHIPS_ADDRESS, CosmoShips.abi, signer);
         const tokenInfo = tokenData.find(token => token.tokenId === tokenId);
         if (!tokenInfo) throw new Error("Token data not found");
 
